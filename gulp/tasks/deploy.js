1361c80baeb8e5e352647b94e8f1c97de8fd285c
Denis Radin <dradin@evolutiongaming.com>
2020-08-05 19:57:18 +0200

    Removes browserconfig

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -5,17 +5,17 @@ const gutil = require('gulp-util');
 const args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
-  const remotePath = '/';
-  const conn = ftp.create({
-    /* TODO: set deploying credentials */
-    // host: 'buff.elastictech.org',
-    // user: args.user,
-    // password: args.password,
-    log: gutil.log,
-    parallel: 10,
+  var remotePath = '/';
+  var conn = ftp.create({
+    host: 'gold.elastictech.org',
+    user: args.user,
+    password: args.password,
+    log: gutil.log
   });
 
-  gulp.src(['./build/**/*.*']).pipe(conn.dest(remotePath));
+  gulp.src(['./build/**/*.*'])
+    .pipe(conn.newer(remotePath))
+    .pipe(conn.dest(remotePath));
 
   gulp
     .src(['./sponsors/**/*.*', '!./sponsors/node_modules/**/*.*'])
