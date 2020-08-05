94abe92ae8216a237ffba51b84fa9e06383f296b
Denis Radin <dradin@evolutiongaming.com>
2020-08-05 19:39:36 +0200

    New website

--- new/.storybook/addons.js
+++
@@ -1,3 +0,0 @@
-import "@focus-reactive/storybook-addon-graphcms/register";
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
--- new/.storybook/index.stories.js
+++
@@ -1,6 +0,0 @@
-const conferenceSettings = require('../gulp/conference-settings');
-const {
-  passConferenceSettings,
-} = require('@focus-reactive/graphql-content-layer/dist/content.stories');
-
-passConferenceSettings(conferenceSettings);
